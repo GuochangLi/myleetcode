3e180abb289d036b34e88e28b24d499267cc1b8e
includelgc <3170105421@zju.edu.cn>
2021-03-16 21:17:26 +0800

    preorder

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.tree.TreeNode;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,3 +10,34 @@ public class Main {
     
 }
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    // public List<Integer> preorderTraversal(TreeNode root) {
+    //     List<Integer> nodes = new ArrayList<> ();
+    //     preorder(root, nodes);
+    //     return nodes;
+    // }
+    // public void preorder(TreeNode root, List<Integer> nodes){
+    //     if(root == null){
+    //         return;
+    //     }
+    //     nodes.add(root.val);
+    //     preorder(root.left, nodes);
+    //     preorder(root.right, nodes);
+    // }
+}
