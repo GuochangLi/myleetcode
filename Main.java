6c9e96c96e66bf0a1cfff8ad088c80e7ded373b5
includelgc <3170105421@zju.edu.cn>
2021-04-14 22:25:44 +0800

    not done

--- Main.java
+++ Main.java
@@ -1,8 +1,31 @@
 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(longestPalindrome("babad"));
+       
     }
-    
+    public ListNode reverseList(ListNode head){
+        if(head.next == null)
+            return head;
+    }
+    for(ListNode begin = head, ListNode behind = begin.next; behind != null;   )
 }
 
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
