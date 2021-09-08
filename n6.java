f9c7834158abf454056cbb29d8af84fa6945e4fb
includelgc <3170105421@zju.edu.cn>
2021-09-08 16:26:08 +0800

    n6

--- n6.java
+++ n6.java
@@ -1,9 +1,4 @@
-
-
-public class Main {
-    public static void main(String[] args) {
-       System.out.println(convert("PAYPALISHIRING", 4));
-    }
+public class n6 {
     public static String convert(String s, int numRows){
         int s_len = s.length();
         char[][] out_s = new char[numRows][s_len];
@@ -47,5 +42,29 @@ public class Main {
         }
         return output_s;
     }
-
 }
+/**
+ * class Solution {
+    public String convert(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
+}
+ */
